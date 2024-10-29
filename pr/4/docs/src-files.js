43d90d224343c87920f26179f6bc5f9a73682ddd
rklaehn <rklaehn@users.noreply.github.com>
2024-10-29 17:07:49 +0000

    deploy: 3f14ab08228d1ceb71878a86e15cf3f74514a01c

--- pr/4/docs/src-files.js
+++ pr/4/docs/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["iroh_gossip",["",[["net",[],["handles.rs","util.rs"]],["proto",[],["hyparview.rs","plumtree.rs","state.rs","topic.rs","util.rs"]],["rpc",[],["client.rs","mod.rs","proto.rs"]]],["lib.rs","metrics.rs","net.rs","proto.rs"]]]\
+["iroh_gossip",["",[["net",[],["handles.rs","util.rs"]],["proto",[],["hyparview.rs","plumtree.rs","state.rs","topic.rs","util.rs"]],["rpc",[],["client.rs","proto.rs"]]],["lib.rs","metrics.rs","net.rs","proto.rs","rpc.rs"]]]\
 ]'));
 createSrcSidebar();
