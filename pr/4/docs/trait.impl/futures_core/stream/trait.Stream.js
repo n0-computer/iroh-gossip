45c3a9d98a7a2b4eebb8445409ced1dc2b434a4c
rklaehn <rklaehn@users.noreply.github.com>
2024-10-29 15:49:40 +0000

    deploy: cdd07abc2929213284dc120e7dfc3c43d828f442

---
+++ pr/4/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_gossip":[["impl Stream for <a class=\"struct\" href=\"iroh_gossip/net/struct.GossipReceiver.html\" title=\"struct iroh_gossip::net::GossipReceiver\">GossipReceiver</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_gossip/net/struct.GossipTopic.html\" title=\"struct iroh_gossip::net::GossipTopic\">GossipTopic</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
