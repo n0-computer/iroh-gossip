1df94873f8a88f025bafa73e10b277d832a4baa4
rklaehn <rklaehn@users.noreply.github.com>
2024-11-18 11:03:49 +0000

    deploy: e7e2de831e19083e1c8d4fe51fdef7f5d6ee61ea

---
+++ pr/9/docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["iroh_gossip",["",[["net",[],["handles.rs","util.rs"]],["proto",[],["hyparview.rs","plumtree.rs","state.rs","topic.rs","util.rs"]],["rpc",[],["client.rs","proto.rs"]]],["cli.rs","lib.rs","metrics.rs","net.rs","proto.rs","rpc.rs"]]]\
+]'));
+createSrcSidebar();
