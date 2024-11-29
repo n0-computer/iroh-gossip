f355295db7d1aae46924f1b71400a94798b19aab
rklaehn <rklaehn@users.noreply.github.com>
2024-11-29 11:40:34 +0000

    deploy: e31e4c8a4577cd718c78691966adc9cd237a310b

---
+++ pr/12/docs/trait.impl/iroh_gossip/proto/trait.PeerIdentity.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_gossip":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ pr/12/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_gossip":[["impl Service for <a class=\"struct\" href=\"iroh_gossip/rpc/proto/struct.RpcService.html\" title=\"struct iroh_gossip::rpc::proto::RpcService\">RpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
