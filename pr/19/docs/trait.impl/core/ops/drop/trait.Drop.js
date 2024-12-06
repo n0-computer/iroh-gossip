00eddaa84bdab08ed14975dc9380d4c566485493
ramfox <ramfox@users.noreply.github.com>
2024-12-06 22:41:19 +0000

    deploy: bc682165e51131c7f2ffd62249f9f95053f32939

---
+++ pr/19/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_gossip":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_gossip/net/struct.EventStream.html\" title=\"struct iroh_gossip::net::EventStream\">EventStream</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
