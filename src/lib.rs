d62a192c758bfe395e08f99ce49549a1b049b035
Rüdiger Klaehn <rklaehn@protonmail.com>
2024-12-13 14:09:22 +0200

    use auto docs (#23)
    also fix links in README.md so we don't get a warning for naked links

--- src/lib.rs
+++ src/lib.rs
@@ -7,27 +7,22 @@
 //!
 //! [iroh]: https://docs.rs/iroh
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
-#![cfg_attr(iroh_docsrs, feature(doc_cfg))]
+#![cfg_attr(iroh_docsrs, feature(doc_auto_cfg))]
 
 pub mod metrics;
 #[cfg(feature = "net")]
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "net")))]
 pub mod net;
 
 #[cfg(feature = "net")]
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "net")))]
 #[doc(inline)]
 pub use net::GOSSIP_ALPN as ALPN;
 
 pub mod proto;
 
 #[cfg(feature = "cli")]
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "cli")))]
 pub mod cli;
 #[cfg(feature = "rpc")]
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "rpc")))]
 pub mod rpc;
-#[cfg_attr(iroh_docsrs, doc(cfg(feature = "rpc")))]
 #[cfg(feature = "rpc")]
 pub use rpc::{
     client::Client as RpcClient,
