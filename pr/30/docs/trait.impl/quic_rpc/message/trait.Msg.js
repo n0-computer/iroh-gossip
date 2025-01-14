264b4bf6993939f2228ed15868059616d8a5f86f
ramfox <ramfox@users.noreply.github.com>
2025-01-14 03:05:16 +0000

    deploy: 91619a410f3f72aebe31ece7b9c743c2f47ca0e6

---
+++ pr/30/docs/trait.impl/quic_rpc/message/trait.Msg.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl Msg&lt;<a class=\"struct\" href=\"iroh_gossip/rpc/proto/struct.RpcService.html\" title=\"struct iroh_gossip::rpc::proto::RpcService\">RpcService</a>&gt; for <a class=\"struct\" href=\"iroh_gossip/rpc/proto/struct.SubscribeRequest.html\" title=\"struct iroh_gossip::rpc::proto::SubscribeRequest\">SubscribeRequest</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[343]}
