e8d73cb6ec29f9900ae79070f9cbaccb8ec577c6
ramfox <ramfox@users.noreply.github.com>
2025-02-25 15:56:46 +0000

    deploy: 36a94d567340db5c64729d9369ea04b3a4bf745c

---
+++ pr/40/docs/iroh_gossip/net/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["GOSSIP_ALPN"],"enum":["Command","Error","Event","GossipEvent"],"mod":["util"],"struct":["Builder","EventStream","Gossip","GossipReceiver","GossipSender","GossipTopic","JoinOptions","Message"],"type":["CommandStream","ProtoCommand","ProtoEvent"]};
