0a06bd22e2f1fff9276b739cc4df5320620b8565
Frando <Frando@users.noreply.github.com>
2025-03-20 10:10:00 +0000

    deploy: a01e2251334dc4cdbf5a3df4a7c477ff00fad611

---
+++ pr/43/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl Subcommand for <a class=\"enum\" href=\"iroh_gossip/cli/enum.GossipCommands.html\" title=\"enum iroh_gossip::cli::GossipCommands\">GossipCommands</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[176]}
---
+++ pr/43/docs/trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"iroh_gossip/proto/enum.DeliveryScope.html\" title=\"enum iroh_gossip::proto::DeliveryScope\">DeliveryScope</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"iroh_gossip/proto/enum.Scope.html\" title=\"enum iroh_gossip::proto::Scope\">Scope</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"iroh_gossip/proto/state/struct.TopicId.html\" title=\"struct iroh_gossip::proto::state::TopicId\">TopicId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[843]}
