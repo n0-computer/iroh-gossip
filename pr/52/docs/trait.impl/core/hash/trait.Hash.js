cb6ebd64418cd82e8282e98b7a455302eeb12565
Frando <Frando@users.noreply.github.com>
2025-04-03 09:35:24 +0000

    deploy: 65d8204305a02b0ebb473f306219585e894a656e

--- pr/52/docs/trait.impl/core/hash/trait.Hash.js
+++ pr/52/docs/trait.impl/core/hash/trait.Hash.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_gossip",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"iroh_gossip/proto/state/struct.TopicId.html\" title=\"struct iroh_gossip::proto::state::TopicId\">TopicId</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"iroh_gossip/proto/state/struct.TopicId.html\" title=\"struct iroh_gossip::proto::state::TopicId\">TopicId</a>"],["impl&lt;PI: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"iroh_gossip/proto/tests/struct.ConnId.html\" title=\"struct iroh_gossip::proto::tests::ConnId\">ConnId</a>&lt;PI&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[297]}
+//{"start":57,"fragment_lengths":[725]}
