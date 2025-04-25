3c205e06d2faeaad4de82745d39c744f09c64551
Philipp Krüger <philipp.krueger1@gmail.com>
2025-04-25 11:21:33 +0200

    docs: Remove outdated comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,5 @@
 #![cfg_attr(feature = "net", doc = include_str!("../README.md"))]
-//! Broadcast messages to peers subscribed to a topic
-//!
-//! The crate is designed to be used from the [iroh] crate, which provides a
-//! [high level interface](https://docs.rs/iroh/latest/iroh/client/gossip/index.html),
-//! but can also be used standalone.
+//! Broadcast messages to peers subscribed to a topic using [iroh].
 //!
 //! [iroh]: https://docs.rs/iroh
 #![deny(missing_docs, rustdoc::broken_intra_doc_links)]
