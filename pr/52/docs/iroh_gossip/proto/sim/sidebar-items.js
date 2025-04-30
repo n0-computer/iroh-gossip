3eb5e3c022c8e76cc64499ee41ba52d3918b718a
Frando <Frando@users.noreply.github.com>
2025-04-30 07:31:17 +0000

    deploy: 3fc40cecc4d6c61aab7809041f571c46bbc2a391

--- pr/52/docs/iroh_gossip/proto/sim/sidebar-items.js
+++ pr/52/docs/iroh_gossip/proto/sim/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BootstrapMode","LatencyConfig"],"struct":["Network","NetworkConfig","NetworkReport","NetworkStats","RoundStats","RoundStatsAvg","RoundStatsAvgDiff","RoundStatsDiff","Simulator","SimulatorConfig"],"type":["PeerId"]};
+window.SIDEBAR_ITEMS = {"enum":["BootstrapMode","LatencyConfig"],"struct":["Network","NetworkConfig","NetworkHistograms","NetworkReport","RoundStats","RoundStatsAvg","RoundStatsAvgDiff","RoundStatsDiff","Simulator","SimulatorConfig"]};
