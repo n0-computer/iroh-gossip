5a48d6a8ecc7ef96043a842fc49994bba23369b3
Frando <frando@unbiskant.org>
2025-05-06 15:14:44 +0200

    remove old quic-rpc protocol

--- src/lib.rs
+++ src/lib.rs
@@ -18,10 +18,3 @@ pub mod net;
 pub use net::GOSSIP_ALPN as ALPN;
 
 pub mod proto;
-#[cfg(feature = "rpc")]
-pub mod rpc;
-#[cfg(feature = "rpc")]
-pub use rpc::{
-    client::Client as RpcClient,
-    proto::{Request as RpcRequest, Response as RpcResponse, RpcService},
-};
