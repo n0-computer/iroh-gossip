677934944eb4b15bc5b59dd6a4f508f03fb0724d
Frando <Frando@users.noreply.github.com>
2025-05-06 17:29:09 +0000

    deploy: 82b52a515788cea7c96b222a439448d0bac228fd

---
+++ pr/67/docs/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["iroh_gossip",["",[["net",[],["handles.rs","rpc.rs","util.rs"]],["proto",[],["hyparview.rs","plumtree.rs","sim.rs","state.rs","topic.rs","util.rs"]]],["lib.rs","metrics.rs","net.rs","proto.rs"]]],["sim",["",[],["sim.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[196,27]}
---
+++ pr/67/docs/trait.impl/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl Stream for <a class=\"struct\" href=\"iroh_gossip/net/struct.GossipReceiver.html\" title=\"struct iroh_gossip::net::GossipReceiver\">GossipReceiver</a>"],["impl Stream for <a class=\"struct\" href=\"iroh_gossip/net/struct.GossipTopic.html\" title=\"struct iroh_gossip::net::GossipTopic\">GossipTopic</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[330]}
