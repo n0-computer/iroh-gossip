2a372144b08f6db43f67536e8694659b4b326698
Franz Heinzmann <frando@unbiskant.org>
2025-05-07 08:23:10 +0200

    refactor!: use new iroh-metrics version, no more global tracking (#58)
    ## Description
    
    Adapts iroh-metrics for https://github.com/n0-computer/iroh-metrics/pull/15 and https://github.com/n0-computer/iroh/pull/3262
    
    Depends on https://github.com/n0-computer/iroh/pull/3262
    
    ## Breaking Changes
    
    
    * `metrics::Metrics` now implements `MetricsGroup` from the  ìroh-metrics@0.34`
    * Metrics are no longer tracked into the `static_core` from `iroh-metrics`, but instead are tracked per `Gossip` and exposed via `Gossip::metrics`
    * `proto::state::State::handle` now takes `Option<&Metrics>` as new 4th parameter
    
    ## Notes & open questions
    
    <!-- Any notes, remarks or open questions you have to make about the PR. -->
    
    ## Change checklist
    
    - [x] Self-review.
    - [x] Documentation updates following the [style guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text), if relevant.
    - [x] Tests if relevant.
    - [x] All breaking changes documented.

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,69 +1,45 @@
 //! Metrics for iroh-gossip
 
-use iroh_metrics::{
-    core::{Counter, Metric},
-    struct_iterable::Iterable,
-};
+use iroh_metrics::{Counter, MetricsGroup};
 
 /// Enum of metrics for the module
-#[allow(missing_docs)]
-#[derive(Debug, Clone, Iterable)]
+#[derive(Debug, Default, MetricsGroup)]
+#[metrics(name = "gossip")]
 pub struct Metrics {
+    /// Number of control messages sent
     pub msgs_ctrl_sent: Counter,
+    /// Number of control messages received
     pub msgs_ctrl_recv: Counter,
+    /// Number of data messages sent
     pub msgs_data_sent: Counter,
+    /// Number of data messages received
     pub msgs_data_recv: Counter,
+    /// Total size of all data messages sent
     pub msgs_data_sent_size: Counter,
+    /// Total size of all data messages received
     pub msgs_data_recv_size: Counter,
+    /// Total size of all control messages sent
     pub msgs_ctrl_sent_size: Counter,
+    /// Total size of all control messages received
     pub msgs_ctrl_recv_size: Counter,
+    /// Number of times we connected to a peer
     pub neighbor_up: Counter,
+    /// Number of times we disconnected from a peer
     pub neighbor_down: Counter,
+    /// Number of times the main actor loop ticked
     pub actor_tick_main: Counter,
+    /// Number of times the actor ticked for a message received
     pub actor_tick_rx: Counter,
+    /// Number of times the actor ticked for an endpoint event
     pub actor_tick_endpoint: Counter,
+    /// Number of times the actor ticked for a dialer event
     pub actor_tick_dialer: Counter,
+    /// Number of times the actor ticked for a successful dialer event
     pub actor_tick_dialer_success: Counter,
+    /// Number of times the actor ticked for a failed dialer event
     pub actor_tick_dialer_failure: Counter,
+    /// Number of times the actor ticked for an incoming event
     pub actor_tick_in_event_rx: Counter,
+    /// Number of times the actor ticked for a timer event
     pub actor_tick_timers: Counter,
-}
-
-impl Default for Metrics {
-    fn default() -> Self {
-        Self {
-            msgs_ctrl_sent: Counter::new("Number of control messages sent"),
-            msgs_ctrl_recv: Counter::new("Number of control messages received"),
-            msgs_data_sent: Counter::new("Number of data messages sent"),
-            msgs_data_recv: Counter::new("Number of data messages received"),
-            msgs_data_sent_size: Counter::new("Total size of all data messages sent"),
-            msgs_data_recv_size: Counter::new("Total size of all data messages received"),
-            msgs_ctrl_sent_size: Counter::new("Total size of all control messages sent"),
-            msgs_ctrl_recv_size: Counter::new("Total size of all control messages received"),
-            neighbor_up: Counter::new("Number of times we connected to a peer"),
-            neighbor_down: Counter::new("Number of times we disconnected from a peer"),
-            actor_tick_main: Counter::new("Number of times the main actor loop ticked"),
-            actor_tick_rx: Counter::new("Number of times the actor ticked for a message received"),
-            actor_tick_endpoint: Counter::new(
-                "Number of times the actor ticked for an endpoint event",
-            ),
-            actor_tick_dialer: Counter::new("Number of times the actor ticked for a dialer event"),
-            actor_tick_dialer_success: Counter::new(
-                "Number of times the actor ticked for a successful dialer event",
-            ),
-            actor_tick_dialer_failure: Counter::new(
-                "Number of times the actor ticked for a failed dialer event",
-            ),
-            actor_tick_in_event_rx: Counter::new(
-                "Number of times the actor ticked for an incoming event",
-            ),
-            actor_tick_timers: Counter::new("Number of times the actor ticked for a timer event"),
-        }
-    }
-}
-
-impl Metric for Metrics {
-    fn name() -> &'static str {
-        "gossip"
-    }
 }
