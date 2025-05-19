4b497e186bb6a4d08c0dab06364e731e270b2831
Frando <Frando@users.noreply.github.com>
2025-05-19 19:23:24 +0000

    deploy: 9427f4b14fff279fb1ad7f042e38d2fc606b1d3c

---
+++ pr/69/docs/trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl Subcommand for <a class=\"enum\" href=\"iroh_gossip/cli/enum.GossipCommands.html\" title=\"enum iroh_gossip::cli::GossipCommands\">GossipCommands</a>"]]],["sim",[["impl Subcommand for <a class=\"enum\" href=\"sim/enum.Command.html\" title=\"enum sim::Command\">Command</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[176,123]}
