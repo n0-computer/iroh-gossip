d933b0cc62f8418b56f377eedf0f1ffbd1b5047f
Frando <Frando@users.noreply.github.com>
2025-06-07 11:46:37 +0000

    deploy: 496d614109b032dc06e4f4053c3a4d61a8bb8d7d

---
+++ pr/75/docs/trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iroh_gossip/net/enum.Error.html\" title=\"enum iroh_gossip::net::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iroh_gossip/net/handles/enum.ApiError.html\" title=\"enum iroh_gossip::net::handles::ApiError\">ApiError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iroh_gossip/net/util/enum.ReadError.html\" title=\"enum iroh_gossip::net::util::ReadError\">ReadError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iroh_gossip/net/util/enum.WriteError.html\" title=\"enum iroh_gossip::net::util::WriteError\">WriteError</a>"]]],["sim",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"sim/enum.SimulationError.html\" title=\"enum sim::SimulationError\">SimulationError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1116,271]}
