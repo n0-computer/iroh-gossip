fd7d635a4bdb0c50f3f247c9585936498ef432f5
Frando <Frando@users.noreply.github.com>
2025-06-20 12:20:32 +0000

    deploy: 5110bea86e6e88c1fd6cce77b405b750297cab28

--- pr/77/docs/trait.impl/snafu/trait.ErrorCompat.js
+++ pr/77/docs/trait.impl/snafu/trait.ErrorCompat.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_gossip",[["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/api/enum.ApiError.html\" title=\"enum iroh_gossip::api::ApiError\">ApiError</a>"],["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/net/enum.Error.html\" title=\"enum iroh_gossip::net::Error\">Error</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/api/enum.ApiError.html\" title=\"enum iroh_gossip::api::ApiError\">ApiError</a>"],["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/net/enum.Error.html\" title=\"enum iroh_gossip::net::Error\">Error</a>"],["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/net/util/enum.ReadError.html\" title=\"enum iroh_gossip::net::util::ReadError\">ReadError</a>"],["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/net/util/enum.WriteError.html\" title=\"enum iroh_gossip::net::util::WriteError\">WriteError</a>"]]],["sim",[["impl ErrorCompat for <a class=\"enum\" href=\"sim/enum.SimulationError.html\" title=\"enum sim::SimulationError\">SimulationError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[292]}
+//{"start":57,"fragment_lengths":[607,148]}
