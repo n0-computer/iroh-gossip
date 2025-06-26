2b360d1071a87dc1695d44706017888e76022502
“ramfox” <kasey@n0.computer>
2025-06-26 17:36:50 -0400

    chore: fmt

--- examples/chat.rs
+++ examples/chat.rs
@@ -93,7 +93,10 @@ async fn main() -> Result<()> {
         None => SecretKey::generate(rand::rngs::OsRng),
         Some(key) => key.parse()?,
     };
-    println!("> our secret key: {}", data_encoding::HEXLOWER.encode(&secret_key.to_bytes()));
+    println!(
+        "> our secret key: {}",
+        data_encoding::HEXLOWER.encode(&secret_key.to_bytes())
+    );
 
     // configure our relay map
     let relay_mode = match (args.no_relay, args.relay) {
