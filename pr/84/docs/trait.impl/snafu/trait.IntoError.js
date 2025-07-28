988a79082d3c38ebc5c3f1c5cc57ae59478f0b40
Frando <Frando@users.noreply.github.com>
2025-07-28 09:54:08 +0000

    deploy: 45a62f223edad921a781d7de2dc9000428bc6c5e

---
+++ pr/84/docs/trait.impl/snafu/trait.IntoError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sim",[["impl IntoError&lt;<a class=\"enum\" href=\"sim/enum.SimulationError.html\" title=\"enum sim::SimulationError\">SimulationError</a>&gt; for <a class=\"struct\" href=\"sim/struct.FailedToBootstrapSnafu.html\" title=\"struct sim::FailedToBootstrapSnafu\">FailedToBootstrapSnafu</a><div class=\"where\">where\n    <a class=\"enum\" href=\"sim/enum.SimulationError.html\" title=\"enum sim::SimulationError\">SimulationError</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> + ErrorCompat,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[593]}
