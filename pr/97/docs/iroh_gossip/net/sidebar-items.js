a31fd2299e95050effedd7cd39cc4e612e1dcc75
Frando <Frando@users.noreply.github.com>
2025-09-23 09:39:50 +0000

    deploy: 24101bd696675d1a927039911c82fd0056a7e598

---
+++ pr/97/docs/iroh_gossip/net/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["GOSSIP_ALPN"],"struct":["ActorStoppedError","Builder","Gossip"]};
---
+++ pr/97/docs/trait.impl/snafu/trait.ErrorCompat.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl ErrorCompat for <a class=\"enum\" href=\"iroh_gossip/api/enum.ApiError.html\" title=\"enum iroh_gossip::api::ApiError\">ApiError</a>"],["impl ErrorCompat for <a class=\"struct\" href=\"iroh_gossip/net/struct.ActorStoppedError.html\" title=\"struct iroh_gossip::net::ActorStoppedError\">ActorStoppedError</a>"]]],["sim",[["impl ErrorCompat for <a class=\"enum\" href=\"sim/enum.SimulationError.html\" title=\"enum sim::SimulationError\">SimulationError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[334,148]}
