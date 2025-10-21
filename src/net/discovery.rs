f7e3ef478a1c4f1ea934e29f3436582e68de734c
Franz Heinzmann <frando@unbiskant.org>
2025-10-21 13:59:59 +0200

    refactor: Use discovery service instead of `Endpoint::add_node_addr` (#108)
    ## Description
    
    iroh-gossip includes a node's addressing information in all Join and ForwardJoin messages (in the peer_data field). This is used by receives of the messages to be able to dial the sender directly, even when not using external discovery mechanisms.
    
    We are using `Endpoint::add_node_addr_with_source` to add the received addr info to the endpoint. In iroh 0.93, `Endpoint::add_node_addr` was removed, and `add_node_addr_with_source` *should* have been removed as well, but was overlooked, and thus iroh-gossip 0.93 only works like previously right now because of an oversight in https://github.com/n0-computer/iroh/pull/3338. We will remove `add_node_addr_with_source` in 0.94 and maybe already deprecate sooner.
    
    This PR changes iroh-gossip to not use `Endpoint::add_node_addr_with_source` anymore and instead get the received addressing info to the endpoint through a discoery service.
    
    The discovery service is fully internal atm: It is not exposed, only added to the endpoint and used from within the crate.
    
    I first used a `StaticProvider` but that would keep the records forever, which is a memory leak for a long-running busy gossip node.  So I switched to an impl that evicts entries after some time.
    
    Right now I hard-coded the retention to 5 minutes, evicting expired entries every 30s. Should this be configurable? Maybe yes, I think.
    
    ## Breaking Changes
    
    <!-- Optional, if there are any breaking changes document them, including how to migrate older code. -->
    
    ## Notes & open questions
    
    Should the retention settings be configurable? Could quite easily add it to the builder.
    
    Previously, this "retention" was fully outside the control of either iroh-gossip or the user, as it was handled by iroh's magic socket internal pruning of unused entries from the node map. Would need to look up what intervals that are.
    
    
    
    ## Change checklist
    
    - [x] Self-review.
    - [x] Documentation updates following the [style guide](https://rust-lang.github.io/rfcs/1574-more-api-documentation-conventions.html#appendix-a-full-conventions-text), if relevant.
    - [x] Tests if relevant.
    - [x] All breaking changes documented.

---
+++ src/net/discovery.rs
@@ -0,0 +1,175 @@
+//! A discovery service to gather addressing info collected from gossip Join and ForwardJoin messages.
+
+use std::{
+    collections::{btree_map::Entry, BTreeMap},
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+
+use iroh::discovery::{Discovery, DiscoveryError, DiscoveryItem, NodeData, NodeInfo};
+use iroh_base::NodeId;
+use n0_future::{
+    boxed::BoxStream,
+    stream::{self, StreamExt},
+    task::AbortOnDropHandle,
+    time::SystemTime,
+};
+
+pub(crate) struct RetentionOpts {
+    /// How long to keep received node info records alive before pruning them
+    retention: Duration,
+    /// How often to check for expired entries
+    evict_interval: Duration,
+}
+
+impl Default for RetentionOpts {
+    fn default() -> Self {
+        Self {
+            retention: Duration::from_secs(60 * 5),
+            evict_interval: Duration::from_secs(30),
+        }
+    }
+}
+
+/// A static node discovery that expires nodes after some time.
+///
+/// It is added to the endpoint when constructing a gossip instance, and the gossip actor
+/// then adds node addresses as received with Join or ForwardJoin messages.
+#[derive(Debug, Clone)]
+pub(crate) struct GossipDiscovery {
+    nodes: NodeMap,
+    _task_handle: Arc<AbortOnDropHandle<()>>,
+}
+
+type NodeMap = Arc<RwLock<BTreeMap<NodeId, StoredNodeInfo>>>;
+
+#[derive(Debug)]
+struct StoredNodeInfo {
+    data: NodeData,
+    last_updated: SystemTime,
+}
+
+impl Default for GossipDiscovery {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl GossipDiscovery {
+    const PROVENANCE: &'static str = "gossip";
+
+    /// Creates a new gossip discovery instance.
+    pub(crate) fn new() -> Self {
+        Self::with_opts(Default::default())
+    }
+
+    pub(crate) fn with_opts(opts: RetentionOpts) -> Self {
+        let nodes: NodeMap = Default::default();
+        let task = {
+            let nodes = Arc::downgrade(&nodes);
+            n0_future::task::spawn(async move {
+                let mut interval = n0_future::time::interval(opts.evict_interval);
+                loop {
+                    interval.tick().await;
+                    let Some(nodes) = nodes.upgrade() else {
+                        break;
+                    };
+                    let now = SystemTime::now();
+                    nodes.write().expect("poisoned").retain(|_k, v| {
+                        let age = now.duration_since(v.last_updated).unwrap_or(Duration::MAX);
+                        age <= opts.retention
+                    });
+                }
+            })
+        };
+        Self {
+            nodes,
+            _task_handle: Arc::new(AbortOnDropHandle::new(task)),
+        }
+    }
+
+    /// Augments node addressing information for the given node ID.
+    ///
+    /// The provided addressing information is combined with the existing info in the static
+    /// provider.  Any new direct addresses are added to those already present while the
+    /// relay URL is overwritten.
+    pub(crate) fn add(&self, node_info: impl Into<NodeInfo>) {
+        let last_updated = SystemTime::now();
+        let NodeInfo { node_id, data } = node_info.into();
+        let mut guard = self.nodes.write().expect("poisoned");
+        match guard.entry(node_id) {
+            Entry::Occupied(mut entry) => {
+                let existing = entry.get_mut();
+                existing
+                    .data
+                    .add_direct_addresses(data.direct_addresses().iter().copied());
+                existing.data.set_relay_url(data.relay_url().cloned());
+                existing.data.set_user_data(data.user_data().cloned());
+                existing.last_updated = last_updated;
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(StoredNodeInfo { data, last_updated });
+            }
+        }
+    }
+}
+
+impl Discovery for GossipDiscovery {
+    fn resolve(&self, node_id: NodeId) -> Option<BoxStream<Result<DiscoveryItem, DiscoveryError>>> {
+        let guard = self.nodes.read().expect("poisoned");
+        let info = guard.get(&node_id)?;
+        let last_updated = info
+            .last_updated
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("time drift")
+            .as_micros() as u64;
+        let item = DiscoveryItem::new(
+            NodeInfo::from_parts(node_id, info.data.clone()),
+            Self::PROVENANCE,
+            Some(last_updated),
+        );
+        Some(stream::iter(Some(Ok(item))).boxed())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use iroh::{discovery::Discovery, NodeAddr, SecretKey};
+    use n0_future::StreamExt;
+    use rand::SeedableRng;
+
+    use super::{GossipDiscovery, RetentionOpts};
+
+    #[tokio::test]
+    async fn test_retention() {
+        let opts = RetentionOpts {
+            evict_interval: Duration::from_millis(100),
+            retention: Duration::from_millis(500),
+        };
+        let disco = GossipDiscovery::with_opts(opts);
+
+        let rng = &mut rand_chacha::ChaCha12Rng::seed_from_u64(1);
+        let k1 = SecretKey::generate(rng);
+        let a1 = NodeAddr::new(k1.public());
+
+        disco.add(a1);
+
+        assert!(matches!(
+            disco.resolve(k1.public()).unwrap().next().await,
+            Some(Ok(_))
+        ));
+
+        tokio::time::sleep(Duration::from_millis(200)).await;
+
+        assert!(matches!(
+            disco.resolve(k1.public()).unwrap().next().await,
+            Some(Ok(_))
+        ));
+
+        tokio::time::sleep(Duration::from_millis(700)).await;
+
+        assert!(disco.resolve(k1.public()).is_none());
+    }
+}
