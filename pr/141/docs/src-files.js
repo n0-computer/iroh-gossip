79f193996e7e9dd28ed70f347c050bede35e162b
Frando <Frando@users.noreply.github.com>
2026-05-08 09:21:21 +0000

    deploy: db1a9b3ce93f688a0c6a1b5a858ea5dd2bfda02d

---
+++ pr/141/docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["iroh_gossip",["",[["net",[],["address_lookup.rs","util.rs"]],["proto",[],["hyparview.rs","plumtree.rs","sim.rs","state.rs","topic.rs","util.rs"]]],["api.rs","lib.rs","metrics.rs","net.rs","proto.rs"]]],["sim",["",[],["sim.rs"]]]]');
+//{"start":19,"fragment_lengths":[203,27]}
---
+++ pr/141/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sim",[["impl Args for <a class=\"struct\" href=\"sim/struct.Cli.html\" title=\"struct sim::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[110]}
