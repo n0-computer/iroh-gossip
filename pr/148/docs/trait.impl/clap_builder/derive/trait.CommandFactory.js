4e3de9b0abc214e1eea6f224dd762661bb3be542
Frando <Frando@users.noreply.github.com>
2026-06-15 13:02:20 +0000

    deploy: 99fab55b6b275c53bcf848247afd090fe9930209

---
+++ pr/148/docs/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sim",[["impl CommandFactory for <a class=\"enum\" href=\"sim/enum.Command.html\" title=\"enum sim::Command\">Command</a>"],["impl CommandFactory for <a class=\"struct\" href=\"sim/struct.Cli.html\" title=\"struct sim::Cli\">Cli</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[237]}
---
+++ pr/148/docs/trait.impl/iroh_metrics/base/trait.MetricsGroup.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_gossip",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_gossip/metrics/struct.Metrics.html\" title=\"struct iroh_gossip::metrics::Metrics\">Metrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[171]}
